70a00fc26695f9afb62243eb42607c1020429131
pasupula priyanka <pasupulapriyanka123@gmail.com>
2020-11-27 12:41:11 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
        <h1>Hello World!!</h1>
        <h1>I'm Priyanka :)</h1>
+       <h1>Student at G. Narayanamma Institute of technolgy and science</h1>
     </div>
   );
 }
